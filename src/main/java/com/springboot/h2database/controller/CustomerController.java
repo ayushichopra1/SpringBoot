1e1850fd6dc8c67b1f4e24e744654e9065a86e37
ayushi.chopra <ayushi.chopra@impressico.com>
2022-02-27 01:23:31 +0530

    initial commit

---
+++ src/main/java/com/springboot/h2database/controller/CustomerController.java
@@ -0,0 +1,93 @@
+package com.springboot.h2database.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.h2database.entity.Customer;
+import com.springboot.h2database.repository.ICustomerRepo;
+
+@RestController
+public class CustomerController {
+
+	@Autowired
+	ICustomerRepo customerRepo;
+
+	@PostMapping("/customers")
+	public ResponseEntity<Customer> save(@RequestBody Customer customer) {
+		try {
+			return new ResponseEntity<>(customerRepo.save(customer), HttpStatus.CREATED);
+
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+
+	@GetMapping("/customers")
+	public ResponseEntity<List<Customer>> getAllCustomers() {
+		try {
+			List<Customer> list = customerRepo.findAll();
+			if (list.isEmpty() || list.size() == 0) {
+				return new ResponseEntity<List<Customer>>(HttpStatus.NO_CONTENT);
+			} else {
+				return new ResponseEntity<List<Customer>>(list, HttpStatus.OK);
+			}
+
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping("/customers/{id}")
+	public ResponseEntity<Customer> getSingleCustomer(@PathVariable long id) {
+		Optional<Customer> customer = customerRepo.findById(id);
+		if (customer.isPresent()) {
+			return new ResponseEntity<Customer>(customer.get(), HttpStatus.OK);
+
+		} else {
+
+			return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+		}
+
+	}
+	
+	@PutMapping("/customers/{id}")
+	public ResponseEntity<Customer> updateCustomer(@RequestBody Customer customer ,@PathVariable Long id) 
+	{
+		Optional<Customer> record = customerRepo.findById(id);
+		if (record.isPresent())
+		{
+			Customer c = record.get();
+			c.setName(customer.getName());
+			c.setAge(customer.getAge());
+			c.setLocation(customer.getLocation());
+			return new ResponseEntity<>(customerRepo.save(c),HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	  
+	@DeleteMapping("/customers/{id}") 
+	public ResponseEntity<HttpStatus> deleteCustomer(@PathVariable long id){ 
+		try { 
+			Optional<Customer> customer =customerRepo.findById(id); 
+			if(customer.isPresent()) { 
+				customerRepo.delete(customer.get()); 
+			} 
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) { 
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); }  }
+
+
+}
